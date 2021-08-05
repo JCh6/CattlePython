dc67e3f97fb4448cc62c1abf907ad2db6e099b2c
JCh6 <juanch@outlook.com>
2021-08-04 22:38:58 -0500

    Generator completed

--- src/usr/prefs.py
+++ src/usr/prefs.py
@@ -2,13 +2,13 @@
 
 prefs = {}
 
-prefs["FILES"]        = { "Cow1": "CowSample.txt"}
+prefs["FILES"]        = { "Cow1": "clean/Cow1.txt"}
 prefs["COLUMNS"]      = { 0: "Lat", 1: "Lon", 2: "Time", 3: "Date" }
 prefs["LAT_KEY"]      = "Lat"
 prefs["LON_KEY"]      = "Lon"
 prefs["DATE_KEY"]     = "Date"
 prefs["MAP_FILENAME"] = "heatmap"
-prefs["SEPARATOR"]    = ";"
+prefs["SEPARATOR"]    = ","
 prefs["CENTER"]       = [4.76994, -74.23248]
 prefs["ZOOM"]         = 20
 
